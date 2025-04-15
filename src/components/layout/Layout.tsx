753942fb6caee65c4d03e44393448773c65f668f
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2025-04-15 10:24:27 -0300

    .

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../Header/Header";
-import Menu from "../../components/Menu/Menu";
+import Menu from "../Menu";
 export default function Layout() {
   return (
     <div className="flex flex-col min-h-screen w-full pl-5 pr-5">
